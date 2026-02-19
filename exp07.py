3392a6d5a3c8dfa5f9d660507adba3c623469e12
GopalGodara <ggskills0g@gmail.com>
2026-02-19 11:37:34 +0530

    Add maximum subarray sum calculation
    Implement a function to find the maximum sum of a subarray.

---
+++ exp07.py
@@ -0,0 +1,13 @@
+# You are given an integer array arr[]. You need to find the maximum sum of a subarray (containing at least one element) in the array arr[].
+# Note : A subarray is a continuous part of an array.
+
+#n = int(input("Enter number of elements: "))
+# arr = []
+# for i in range(n):
+#     arr.append(int(input(f"Enter element {i+1}: ")))
+# max_sum = arr[0]   
+# current_sum = arr[0]   
+# for num in arr[1:]:
+#     current_sum = max(num, current_sum + num)
+#     max_sum = max(max_sum, current_sum)
+# print("Maximum subarray sum:", max_sum)
