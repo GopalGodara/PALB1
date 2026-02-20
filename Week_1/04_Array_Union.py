78a6fac583dd4ba688234fe8733b93cb3d39f420
GopalGodara <ggskills0g@gmail.com>
2026-02-20 17:58:17 +0530

    Add files via upload

---
+++ Week_1/04_Array_Union.py
@@ -0,0 +1,8 @@
+# You are given two arrays a[] and b[], return the Union of both the arrays in anyorder.
+# The Union of two arrays is a collection of all distinct elements present in either of the arrays. If an element appears more than once in one or both arrays, it should be
+# included only once in the result.
+# Note: Elements of a[] and b[] are not necessarily distinct.
+# Note that, You can return the Union in any order but the driver code will print the result in sorted order only.
+
+# take home ( to be done by today)
+
