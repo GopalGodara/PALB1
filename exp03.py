3205868a0ff2fa64606bcf10edc8e91d60a18078
GopalGodara <ggskills0g@gmail.com>
2026-02-20 17:59:55 +0530

    Delete exp03.py

--- exp03.py
+++
@@ -1,8 +0,0 @@
-#Given an integer array arr[] and an integer k, your task is to find and return the kth smallest element in the given array.
-# Note: The kth smallest element is determined based on the sorted order of thearray.
-
-
-
-
-# take home (to be done by today)
-
